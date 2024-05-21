731cba1e71a86da8e469afb55788234e37b03082
ruman <ruman@mail.ru>
2024-05-21 16:08:57 +0300

    5555

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let age = 40;
+console.log(age)
+
+let message = "привет мир!";
+console.log(message)
+
+var a = 50;
+console.log(a)
+var b = "привет земля";
+  console.log(b)
+
+var  phone  = prompt('Номер телефона','');
+console.log('ваш номер' + phone);
+
+var   address  = prompt('Ваш Адрес','');
+console.log('ваш Адрес' +  address);
